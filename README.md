7517382aa206efb1e97af2f17b755ffe73a54949
MggMuggins <mggmugginsmc@gmail.com>
2019-04-03 13:57:17 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Block Renderer
+An attempt to write an isometric block renderer for the AP CSP create task. WIP.
---
+++ faces.py
@@ -0,0 +1,15 @@
+
+def top_face(sprite):
+    pass
+
+def left_face(sprite):
+    pass
+
+"""
+Simply applies a reflection on left_face(sprite)
+"""
+def right_face(sprite):
+    pass
+
+def tesselate():
+    pass
