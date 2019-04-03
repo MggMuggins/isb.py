7edda6c17f7983c27370d63de4d6a73a284ef3e0
MggMuggins <mggmugginsmc@gmail.com>
2019-04-03 14:37:47 -0500

    Top Face transformation

--- faces.py
+++ faces.py
@@ -1,12 +1,25 @@
+import Image
+
 """
 Create just the top face of the cube
 
-sprite - Base square 16*16 texture
+sprite - Base square texture
 width - The width of the isometric top after transformation.
         The height of the returned image will be width / 2.
 """
 def top_face(sprite, width):
-    pass
+    # Resize so that the diagonal is the correct length
+    # given the width that I've got
+    sprite_size = sprite.size_column
+    scale_factor = sqrt(width/2) / sprite_size
+    side_len = sprite_size * scale_factor
+    sprite.resize((side_len, side_len))
+    
+    sprite.rotate(45)
+    
+    sprite.resize((width, width / 2))
+    
+    return sprite
 
 """
 Create the left side of the cube
@@ -21,8 +34,15 @@ def side_face(sprite, height):
 """
 Create a full isometric image
 
-top, left, right - Appropriate 16*16 textures
+top, left, right - Appropriate textures
 size - finished size of the square isometric image
 """
 def tesselate(top, left, right, size):
     pass
+
+if __name__ == "__main__":
+    im = Image.open("dirt.png")
+    
+    top = top_face(im)
+    
+    top.save("dirt_transformed.png")
